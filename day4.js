4398bedb960c157247d91c09a1c1f3a9f6341e04
Jualko <66382948+Jualko@users.noreply.github.com>
2022-12-04 19:21:42 +0100

    Update day4.js

--- day4.js
+++ day4.js
@@ -1,19 +1,20 @@
 const fs = require('fs');
-let aPairs = fs.readFileSync('day4input.txt', 'utf8').split("\r\n").map(e => e.split(","));
+const aPairs = fs.readFileSync('day4input.txt', 'utf8').split("\r\n").map(e => [formatPair(e, 0), formatPair(e, 1)]);
+
+function formatPair(aPair, index) {
+    return aPair.split(",")[index].split("-").map(e => parseInt(e));
+}
 
 //Part 1: In how many assignment pairs does one range fully contain the other?
-
-console.log("Part 1: " + aPairs.filter(e => {
-    const aA = e[0].split("-").map(e => parseInt(e));
-    const aB = e[1].split("-").map(e => parseInt(e));
+function doesContain(aA, aB) {
     return (aA[0] >= aB[0] && aA[1] <= aB[1]) || (aB[0] >= aA[0] && aB[1] <= aA[1]);
-}).length);
+}
+
+console.log("Part 1: " + aPairs.filter(e => doesContain(e[0], e[1])).length);
 
 //Part 2: In how many assignment pairs do the ranges overlap?
-aPairs = aPairs.filter(e => {
-    const aA = e[0].split("-").map(e => parseInt(e));
-    const aB = e[1].split("-").map(e => parseInt(e));
-   return (aA[0] >= aB[0] && aA[1] <= aB[1]) || (aB[0] >= aA[0] && aB[1] <= aA[1]) || (aB[0] <= aA[1] && aB[0] >= aA[0]) || (aA[0] <= aB[1] && aA[0] >= aB[0])
-})
+function doesOverlap(aA, aB) {
+    return doesContain(aA, aB) || (aB[0] <= aA[1] && aB[0] >= aA[0]) || (aA[0] <= aB[1] && aA[0] >= aB[0]);
+}
 
-console.log("Part 2: " + aPairs.length);
+console.log("Part 2: " + aPairs.filter(e => doesOverlap(e[0], e[1])).length);
