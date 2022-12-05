3ba687237f3b2990aeb4ebf1302a71dd9619e369
Jualko <66382948+Jualko@users.noreply.github.com>
2022-12-05 11:36:14 +0100

    Update day5.js

--- day5.js
+++ day5.js
@@ -14,27 +14,37 @@ const aMoves = aInput.slice(iDivider + 1, aInput.length).map(m => {
 const aCrateRows = aInput.slice(0, iDivider - 1);
 const iStacks = Math.floor(aInput[0].length / 4 + 0.25);
 
-let aStacks = [];
-for (let i = 0; i < aCrateRows.length; i++) {
-    for (let j = 0; j < iStacks; j++) {
-        if (aStacks[j] === undefined) {
-            aStacks[j] = [];
-        }
-        const sCrate = aCrateRows[i][j * 4 + 1];
-        if (sCrate && sCrate.trim()) {
-            aStacks[j].unshift(sCrate);
+function createStacks() {
+    let aStacks = [];
+    for (let i = 0; i < aCrateRows.length; i++) {
+        for (let j = 0; j < iStacks; j++) {
+            if (aStacks[j] === undefined) {
+                aStacks[j] = [];
+            }
+            const sCrate = aCrateRows[i][j * 4 + 1];
+            if (sCrate && sCrate.trim()) {
+                aStacks[j].unshift(sCrate);
+            }
         }
     }
+    return aStacks;
 }
 
-for (let i = 0; i < aMoves.length; i++) {
-    const oMove = aMoves[i];
-    for (let j = 0; j < oMove.amount; j++) {
-        aStacks[oMove.to].push(aStacks[oMove.from][aStacks[oMove.from].length - 1]);
-        aStacks[oMove.from].pop();
+function moveCrates9000(aStacks){
+    for (let i = 0; i < aMoves.length; i++) {
+        const oMove = aMoves[i];
+        for (let j = 0; j < oMove.amount; j++) {
+            aStacks[oMove.to].push(aStacks[oMove.from][aStacks[oMove.from].length - 1]);
+            aStacks[oMove.from].pop();
+        }
     }
+    return aStacks;
 }
 
-let sTop = "";
-aStacks.forEach(s => {sTop += s[s.length - 1]})
-console.log("Part 1: " + sTop);
+function getTops(aStacks){
+    let sTops = "";
+    aStacks.forEach(s => { sTops += s[s.length - 1] })
+    return sTops;
+}
+
+console.log("Part 1: " + getTops(moveCrates9000(createStacks())));
