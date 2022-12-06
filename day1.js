ccf78883074f5e06ca896ca018c89926c6290470
Jualko <66382948+Jualko@users.noreply.github.com>
2022-12-06 10:56:41 +0100

    Update day1.js

--- day1.js
+++ day1.js
@@ -3,22 +3,12 @@ const aInput = fs.readFileSync('day1input.txt', 'utf8').split("\r\n").map(e => p
 
 //Part 1: Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
 
-let aElves = [];
-let iCalories = 0;
-for (let i = 0; i < aInput.length; i++) {
-    if (aInput[i] === 0 || i === aInput.length - 1) {
-        aElves.push(iCalories);
-        iCalories = 0;
-    } else {
-        iCalories += aInput[i];
-    }
-}
+let aElves = [0];
+aInput.forEach(i => { i === 0 ? aElves.push(0) : aElves[aElves.length - 1] += i; });
+aElves.sort((a, b) => a - b);
 
-console.log("Part 1: " + Math.max.apply(this, aElves));
+console.log("Part 1: " + aElves[aElves.length - 1]);
 
 //Part 2: Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
 
-let top1 = Math.max.apply(this, aElves);
-let top2 = Math.max.apply(this, aElves.filter((i) => i < top1));
-let top3 = Math.max.apply(this, aElves.filter((i) => i < top2));
-console.log("Part 2: " + (top1 + top2 + top3));
+console.log("Part 2: " + aElves.slice(aElves.length - 3, aElves.length).reduce((a, b) => a + b));
