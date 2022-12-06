77cf318eb5e77869ac6682d1f95dda3198f3ac09
Jualko <66382948+Jualko@users.noreply.github.com>
2022-12-06 12:05:21 +0100

    Update day6.js

--- day6.js
+++ day6.js
@@ -1,23 +1,19 @@
 const fs = require('fs');
 const sDatastream = fs.readFileSync('day6input.txt', 'utf8');
 
-function detectMarker(datastream, markerlength) {
-    let aMarker = [];
-    for (let i = 0; i < datastream.length; i++) {
-        aMarker.push(datastream[i]);
-        if (aMarker.length === markerlength) {
-            if (new Set(aMarker).size === aMarker.length) {
-                return datastream.indexOf(aMarker.join('')) + markerlength;
-            }
-            aMarker.shift();
+function detectMarker(markerlength) {
+    for (let i = 0; i < sDatastream.length; i++) {
+        const aMarker = sDatastream.slice(i, i + markerlength);
+        if (new Set(aMarker).size === aMarker.length) {
+            return sDatastream.indexOf(aMarker) + markerlength;
         }
     }
 }
 
 //Part 1: How many characters need to be processed before the first start-of-packet marker is detected?
 
-console.log("Part 1: " + detectMarker(sDatastream, 4));
+console.log("Part 1: " + detectMarker(4));
 
 //Part 2: How many characters need to be processed before the first start-of-message marker is detected?
 
-console.log("Part 2: " + detectMarker(sDatastream, 14));
+console.log("Part 2: " + detectMarker(14));
