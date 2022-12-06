d6f9fd6fa1d3f2a9eae018d4254ee735ece6a50c
Jualko <66382948+Jualko@users.noreply.github.com>
2022-12-06 11:11:08 +0100

    Update day2.js

--- day2.js
+++ day2.js
@@ -1,49 +1,34 @@
-//Part 1: What would your total score be if everything goes exactly according to your strategy guide?
-
 const fs = require('fs');
-const mPoints = {
-    "X" : 1, //rock
-    "Y" : 2, //paper
-    "Z" : 3, //scissors
-    "AX" : 3,
-    "AY" : 6,
-    "AZ" : 0,
-    "BX" : 0,
-    "BY" : 3,
-    "BZ" : 6,
-    "CX" : 6,
-    "CY" : 0,
-    "CZ" : 3
-}
 const aStrategyGuide = fs.readFileSync('day2input.txt', 'utf8').split("\r\n").map(e => e.split(" "));
 
-let points = 0;
+function calculateScore(mPoints) {
+    return aStrategyGuide.reduce((a, c) => a + mPoints[c[0] + c[1]], 0);
+}
 
-aStrategyGuide.forEach(e => {
-    points += mPoints[e[1]] + (mPoints[e[0] + e[1]]);
-});
+//Part 1: What would your total score be if everything goes exactly according to your strategy guide?
 
-console.log(points);
+console.log("Part 1: " + calculateScore({
+    "AX": 3 + 1,
+    "AY": 6 + 2,
+    "AZ": 0 + 3,
+    "BX": 0 + 1,
+    "BY": 3 + 2,
+    "BZ": 6 + 3,
+    "CX": 6 + 1,
+    "CY": 0 + 2,
+    "CZ": 3 + 3
+}));
 
 //Part 2: Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
 
-const mPoints2 = {
-    //A rock, B paper, C scissors, X lose, Y draw, Z win
-    "AX" : 0+3,
-    "AY" : 3+1,
-    "AZ" : 6+2,
-    "BX" : 0+1,
-    "BY" : 3+2,
-    "BZ" : 6+3,
-    "CX" : 0+2,
-    "CY" : 3+3,
-    "CZ" : 6+1
-}
-
-points = 0;
-
-aStrategyGuide.forEach(e => {
-    points += mPoints2[e[0] + e[1]];
-});
-
-console.log(points);
+console.log("Part 2: " +calculateScore({
+    "AX": 0 + 3,
+    "AY": 3 + 1,
+    "AZ": 6 + 2,
+    "BX": 0 + 1,
+    "BY": 3 + 2,
+    "BZ": 6 + 3,
+    "CX": 0 + 2,
+    "CY": 3 + 3,
+    "CZ": 6 + 1
+}));
