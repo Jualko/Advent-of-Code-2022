f4a3998ee21db7d742863588400f82402031b58a
Jualko <66382948+Jualko@users.noreply.github.com>
2022-12-06 11:41:17 +0100

    Update day3.js

--- day3.js
+++ day3.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const aRucksacks = fs.readFileSync('day3input.txt', 'utf8').split("\r\n");
 
-//Part 1: Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
-
 function getPriority(c) {
     let priority = c.charCodeAt(0) - 96;
     if (priority < 1 || priority > 26) {
@@ -11,37 +9,32 @@ function getPriority(c) {
     return priority;
 }
 
-iSumOfPriorities = 0;
+//Part 1: Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
 
-aRucksacks.map(e => [e.slice(0, e.length / 2), e.slice(e.length / 2, e.length)]).forEach(rucksack => {
-    for (let i = 0; i < rucksack[0].length; i++) {
-        const c = rucksack[0][i];
-        if (rucksack[1].includes(c)) {
-            iSumOfPriorities += getPriority(c);
-            break;
+console.log("Part 1: " + aRucksacks.reduce(reduceRucksack, 0));
+
+function reduceRucksack(a, r) {
+    r = [r.slice(0, r.length / 2), r.slice(r.length / 2, r.length)];
+    for (let i = 0; i < r[0].length; i++) {
+        if (r[1].includes(r[0][i])) {
+            return a + getPriority(r[0][i]);
         }
     }
-})
-
-console.log("Part 1: " + iSumOfPriorities)
+}
 
 //Part 2: Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
 
-aGroups = [];
+let aGroups = [];
 for (let i = 0; i < aRucksacks.length; i += 3) {
-    aGroups.push([aRucksacks[i], aRucksacks[i + 1], aRucksacks[i + 2]]);   
+    aGroups.push(aRucksacks.slice(i, i + 3));
 }
 
-iSumOfPriorities = 0;
+console.log("Part 2: " + aGroups.reduce(reduceGroups, 0));
 
-aGroups.map(group => {
-    for (let i = 0; i < group[0].length; i++) {
-        const c = group[0][i];
-        if (group[1].includes(c) && group[2].includes(c)) {
-            iSumOfPriorities += getPriority(c);
-            break;
+function reduceGroups(a, g) {
+    for (let i = 0; i < g[0].length; i++) {
+        if (g[1].includes(g[0][i]) && g[2].includes(g[0][i])) {
+            return a + getPriority(g[0][i]);
         }
     }
-})
-
-console.log("Part 2: " + iSumOfPriorities)
+}
