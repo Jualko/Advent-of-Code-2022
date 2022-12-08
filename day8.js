b2537b28ac1c504b34f9ba9fbec77ef9616ab1c1
Jualko <66382948+Jualko@users.noreply.github.com>
2022-12-08 16:27:27 +0100

    day8 (part 2)

--- day8.js
+++ day8.js
@@ -3,7 +3,7 @@ const aTreeHeightMap = fs.readFileSync('day8input.txt', 'utf8').split("\r\n").ma
 
 //Part 1: Consider your map; how many trees are visible from outside the grid?
 
-function getCountVisibleTrees(){
+function getCountVisibleTrees() {
     let visible = 0;
     for (let y = 0; y < aTreeHeightMap.length; y++) {
         for (let x = 0; x < aTreeHeightMap[y].length; x++) {
@@ -62,3 +62,66 @@ function isVisibleFromRight(iY, iX) {
 console.log("Part 1: " + getCountVisibleTrees());
 
 //Part 2: Consider each tree on your map. What is the highest scenic score possible for any tree?
+
+function getMaxScenicScore() {
+    let iMax = 1;
+    for (let y = 0; y < aTreeHeightMap.length; y++) {
+        for (let x = 0; x < aTreeHeightMap[y].length; x++) {
+            let iScore = getScenicScore(y, x);
+            if (iScore > iMax) {
+                iMax = iScore;
+            }
+        }
+    }
+    return iMax;
+}
+
+function getScenicScore(iY, iX) {
+    return getScenicScoreFromTop(iY, iX) * getScenicScoreFromBottom(iY, iX) * getScenicScoreFromLeft(iY, iX) * getScenicScoreFromRight(iY, iX);
+}
+
+function getScenicScoreFromTop(iY, iX) {
+    let iScore = 0;
+    for (let i = iY - 1; i >= 0; i--) {
+        iScore++;
+        if (aTreeHeightMap[i][iX] >= aTreeHeightMap[iY][iX]) {
+            break;
+        }
+    }
+    return iScore;
+}
+
+function getScenicScoreFromBottom(iY, iX) {
+    let iScore = 0;
+    for (let i = iY + 1; i < aTreeHeightMap.length; i++) {
+        iScore++;
+        if (aTreeHeightMap[i][iX] >= aTreeHeightMap[iY][iX]) {
+            break;
+        }
+    }
+    return iScore;
+}
+
+function getScenicScoreFromLeft(iY, iX) {
+    let iScore = 0;
+    for (let i = iX - 1; i >= 0; i--) {
+        iScore++;
+        if (aTreeHeightMap[iY][i] >= aTreeHeightMap[iY][iX]) {
+            break;
+        }
+    }
+    return iScore;
+}
+
+function getScenicScoreFromRight(iY, iX) {
+    let iScore = 0;
+    for (let i = iX + 1; i < aTreeHeightMap[iY].length; i++) {
+        iScore++;
+        if (aTreeHeightMap[iY][i] >= aTreeHeightMap[iY][iX]) {
+            break;
+        }
+    }
+    return iScore;
+}
+
+console.log("Part 2: " + getMaxScenicScore());
