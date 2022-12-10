1d38c2225d57e36b320263716690664360ab1b19
Jualko <66382948+Jualko@users.noreply.github.com>
2022-12-10 14:52:25 +0100

    day10 (part 2)

--- day10.js
+++ day10.js
@@ -29,3 +29,36 @@ function getSignalStrengthSumByCycles(...aCycles) {
 }
 
 console.log("Part 1:", getSignalStrengthSumByCycles(20, 60, 100, 140, 180, 220));
+
+//Part 2: Render the image given by your program. What eight capital letters appear on your CRT?
+
+function drawImage() {
+    let iCycle = 1;
+    let x = 1;
+    let sRow = "";
+
+    aInstructions.forEach(e => {
+        let iDuration = { addx: 2, noop: 1 }[e.split(" ")[0]];
+
+        for (let i = 0; i < iDuration; i++) {
+            if ((iCycle - 1) % 40 === x - 1 || (iCycle - 1) % 40 === x || (iCycle - 1) % 40 === x + 1) {
+                sRow += "#";
+            } else {
+                sRow += ".";
+            }
+            if ((iCycle - 1) % 40 === 39) {
+                console.log(sRow)
+                sRow = "";
+            }
+            if (i === iDuration - 1) {
+                if (e.split(" ")[0] === "addx") {
+                    x += parseInt(e.split(" ")[1]);
+                }
+            }
+            iCycle++;
+        }
+    });
+}
+
+console.log("Part 2:");
+drawImage();
