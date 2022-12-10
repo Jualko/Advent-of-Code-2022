331a16d0c8770900aab77ab4f08e2e9e57887505
Jualko <66382948+Jualko@users.noreply.github.com>
2022-12-10 15:30:59 +0100

    Update day10.js

--- day10.js
+++ day10.js
@@ -11,7 +11,7 @@ let sCRT = "";
 
 aInstructions.forEach(e => {
     for (let i = 1; i <= { addx: 2, noop: 1 }[e[0]]; i++, iCycle++) {
-        sCRT += ((iCycle - 1) % 40 >= x - 1 && (iCycle - 1) % 40 <= x + 1 ? "#" : ".") + ((iCycle - 1) % 40 === 39 ? "\n" : "");
+        sCRT += ((iCycle - 1) % 40 >= x - 1 && (iCycle - 1) % 40 <= x + 1 ? "██" : "░░") + ((iCycle - 1) % 40 === 39 ? "\n" : "");
         iSum += [20, 60, 100, 140, 180, 220].includes(iCycle + 1) ? x * iCycle : 0;
         x += (i === { addx: 2, noop: 1 }[e[0]] && e[0] === "addx") ? parseInt(e[1]) : 0;
     }
