21ae076c6b82ba7804e4a7b6efacaf17fd6d21d9
Jualko <66382948+Jualko@users.noreply.github.com>
2022-12-10 15:14:43 +0100

    Update day10.js

--- day10.js
+++ day10.js
@@ -1,32 +1,20 @@
 const fs = require('fs');
-const aInstructions = fs.readFileSync('day10input.txt', 'utf8').split("\r\n");
+const aInstructions = fs.readFileSync('day10input.txt', 'utf8').split("\r\n").map(e => e.split(" "));
 
 //Part 1: Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles. What is the sum of these six signal strengths?
 //Part 2: Render the image given by your program. What eight capital letters appear on your CRT?
 
-const aCycles = [20, 60, 100, 140, 180, 220]
 let iCycle = 1;
 let x = 1;
 let iSum = 0;
-let sRow = "";
+let sCRT = "";
 
 aInstructions.forEach(e => {
-    let iDuration = { addx: 2, noop: 1 }[e.split(" ")[0]];
-    for (let i = 0; i < iDuration; i++) {
-        let iPixel = (iCycle - 1) % 40;
-        sRow += (iPixel >= x - 1 && iPixel <= x + 1) ? "#" : ".";
-        if (iPixel === 39) {
-            console.log(sRow)
-            sRow = "";
-        }
-        if (aCycles.includes(iCycle)) {
-            iSum += x * iCycle;
-        }
-        if (i === iDuration - 1 && e.split(" ")[0] === "addx") {
-            x += parseInt(e.split(" ")[1]);
-        }
-        iCycle++;
+    for (let i = 1; i <= { addx: 2, noop: 1 }[e[0]]; i++, iCycle++) {
+        sCRT += ((iCycle - 1) % 40 >= x - 1 && (iCycle - 1) % 40 <= x + 1 ? "#" : ".") + ((iCycle - 1) % 40 === 39 ? "\n" : "");
+        iSum += [20, 60, 100, 140, 180, 220].includes(iCycle + 1) ? x * iCycle : 0;
+        x += (i === { addx: 2, noop: 1 }[e[0]] && e[0] === "addx") ? parseInt(e[1]) : 0;
     }
 });
-
 console.log("Part 1:", iSum);
+console.log("Part 2:\n" + sCRT);
